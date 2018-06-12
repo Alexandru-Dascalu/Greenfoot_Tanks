e0a9ea90371f45a21ba32992ecaa8931dc359d43
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-06-12 21:39:40 +0100

    Update WallBlock.java

--- WallBlock.java
+++ WallBlock.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WallBlock extends Actor
 {
+	private static final int LENGTH=60;
+	private static final int HEIGHT=60;
+	
     /**
      * Act - do whatever the Wall wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +18,37 @@ public class WallBlock extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+    }
+    
+    public String getQuadrant(int x, int y)
+    {
+    	int diagSlope=HEIGHT/LENGTH;
+    	
+    	int topLeftCornerX=getX()-(LENGTH/2);
+    	int topLeftCornerY=getY()-(HEIGHT/2);
+    	
+    	String[][] quadrants= { {"top","right"}, {"left","bottom"} };
+    	String[] temp;
+    	String corner;
+    	
+    	if(y-topLeftCornerY<=diagSlope*(x-topLeftCornerX))
+    	{
+    		temp=quadrants[0];
+    	}
+    	else
+    	{
+    		temp=quadrants[1];
+    	}
+    	
+    	if(y-topLeftCornerY-HEIGHT<=(-diagSlope)*(x-topLeftCornerX))
+    	{
+    		corner=temp[0];
+    	}
+    	else
+    	{
+    		corner=temp[1];
+    	}
+    	
+    	return corner;
+    }
 }
