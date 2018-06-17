045860575776c36a1afc2b07e3681234015e8ee7
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-06-17 11:31:33 +0100

    Update BrownTank.java

--- BrownTank.java
+++ BrownTank.java
@@ -7,6 +7,14 @@ public class BrownTank extends Tank
 		super(startX,startY);
 	}
 	
+	public void act()
+	{
+		super.act();
+		
+		tankTurret.aim();
+		tankTurret.fire();
+	}
+	
 	@Override
 	public boolean isMoving()
 	{
