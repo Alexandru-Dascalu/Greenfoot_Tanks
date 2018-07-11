8e336c4a5147aaa956fd10085cc36c5a131867da
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-07-11 16:20:23 +0300

    Finished commenting (#34)
    Added comments for the TankWorld, Turret, Target, TargetLine and WallBlock classes .
    
    * Update TankWorld.java
    
    * Update Target.java
    
    * Update TargetLine.java
    
    * Update TargetLine.java
    
    * Update TargetLine.java
    
    * Update WallBlock.java
    
    * Update Turret.java
    
    * Update Shell.java

--- Target.java
+++ Target.java
@@ -1,17 +1,32 @@
 import greenfoot.*;
 
 /**
- * Write a description of class Target here.
+ * <p><b>File name: </b> Target.java
+ * @version 1.0
+ * @since 10.05.2018
+ * <p><p><b>Last modification date: </b> 11.07.2018
+ * @author Alexandru F. Dascalu
+ * <p><b>Copyright: </b>
+ * <p>No copyright.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * <p><b>Purpose: </b>
+ * <p> This class models a player target for a Greenfoot recreation of the Wii 
+ * Play Tanks game for the Nintendo Wii. It is a target that is moved with the
+ * mouse (code for that is in the PlayerTurret class) to help the player aim.
+ * 
+ * <p><b>Version History</b>
+ * <p>	-1.0 - Created the class.
  */
+
 public class Target extends Actor
 {
     /**
      * Act - do whatever the Target wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
+     * The target is just there to be moved by the player's turret, so by itself
+     * it does nothing.
      */
+	@Override
     public void act() 
     {
        
