0c3644ac39eccf50f81775d7c9324370ab1ace40
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-08-07 16:35:27 +0300

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,194 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+/**
+ * Write a description of class Graph here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Graph  
+{
+    private GraphPoint[][] pointMatrix;
+
+    private TankWorld world;
+    private int matrixLength;
+    private int matrixWidth;
+    
+    /**
+     * Constructor for objects of class Graph
+     */
+    public Graph(TankWorld tankWorld)
+    {
+    	world=tankWorld;
+    	matrixLength=(TankWorld.LENGTH-2*WallBlock.SIDE)/GraphPoint.INTERVAL+1;
+    	
+    	matrixWidth=(TankWorld.WIDTH-2*WallBlock.SIDE)/GraphPoint.INTERVAL+1;
+    	
+    	pointMatrix=new GraphPoint[matrixWidth][matrixLength];
+    
+    	int x, y;
+   
+    	for(int i=0;i<matrixWidth;i++)
+    	{
+    		y=WallBlock.SIDE+(GraphPoint.INTERVAL/2)+i*GraphPoint.INTERVAL;
+    		
+    		for(int j=0;j<matrixLength;j++)
+    		{
+    			x=WallBlock.SIDE+(GraphPoint.INTERVAL/2)+j*GraphPoint.INTERVAL;
+    			
+    			if(!GraphPoint.isIntersectingWall(x, y, tankWorld))
+    			{
+    				pointMatrix[i][j]=new GraphPoint(x,y);
+    				
+    				if(i!=0)
+    				{
+    					pointMatrix[i][j].addNeighbour(pointMatrix[i-1][j], "upper");
+    				}
+    				
+    				if(j!=0)
+    				{
+    					pointMatrix[i][j].addNeighbour(pointMatrix[i][j-1], "left");
+    				}
+    				
+    				if(j!=0 && i!=0)
+    				{
+    					pointMatrix[i][j].addNeighbour(pointMatrix[i-1][j-1], "upper left");
+    				}
+    				
+    				if(i!=0 && j!=(matrixLength-1))
+    				{
+    					pointMatrix[i][j].addNeighbour(pointMatrix[i-1][j+1], "upper right");
+    				}
+    			}
+    			else
+    			{
+    				pointMatrix[i][j]=null;
+    			}
+    			
+    		}
+    	}
+    }
+    
+    public GraphPoint getPoint(int rowIndex, int columnIndex)
+    {
+    	return pointMatrix[rowIndex][columnIndex];
+    }
+    
+    public LinkedList<GraphPoint> getShortestPath(int startX, int startY,
+    		GraphPoint target)
+    {
+    	GraphPoint source=getSourcePoint(startX, startY);
+    	PriorityQueue<GraphPoint> unvisitedNodes=new PriorityQueue<>();
+    	
+    	unvisitedNodes.add(source);
+    	for(GraphPoint[] pointRow: pointMatrix)
+    	{
+    		for(GraphPoint point: pointRow)
+    		{
+    			if(point!=null)
+    			{
+    				unvisitedNodes.add(point);
+    			}
+    		}
+    	}
+    	
+    	GraphPoint current;
+    	while(!unvisitedNodes.isEmpty())
+    	{
+    		current=unvisitedNodes.poll();
+    		current.setVisited(true);
+    		if(current.equals(target))
+    		{
+    			break;
+    		}
+    		
+    		if(current.getDistance()==Integer.MAX_VALUE)
+    		{
+    			return null;
+    		}
+    		
+    		for(GraphPoint neighbour: current.getNeighbours())
+    		{
+    			if(neighbour!=null && !neighbour.isVisited())
+    			{
+    				double tentativeDistance = current.getDistance()+
+    						GraphPoint.getDistance(current, neighbour);
+    				
+    				if(tentativeDistance<neighbour.getDistance())
+    				{
+    					unvisitedNodes.remove(neighbour);
+    					neighbour.setDistance(tentativeDistance);
+    					neighbour.setBestPrevious(current);
+    					
+    					unvisitedNodes.add(neighbour);
+    				}
+    				
+    			}
+    		}
+    	}
+    	
+    	LinkedList<GraphPoint> path=getPath(target);
+    	resetGraph();
+    	return path;
+    }
+    
+    private GraphPoint getSourcePoint(int startX, int startY)
+    {
+    	GraphPoint source=new GraphPoint(startX, startY);
+    	source.setDistance(0);
+    	
+    	startX-=WallBlock.SIDE;
+    	startY-=WallBlock.SIDE;
+    	
+    	int rowIndex=startY/GraphPoint.INTERVAL;
+    	int columnIndex=startX/GraphPoint.INTERVAL;
+    	
+    	GraphPoint approximateSource=pointMatrix[rowIndex][columnIndex];
+    	
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("upper left")
+    			, "upper left");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("upper")
+    			, "upper");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("upper right")
+    			, "upper right");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("left"), "left");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("right"), "right");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("lower left")
+    			, "lower left");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("lower"), "lower");
+    	source.addOneWayNeighbour(approximateSource.getNeighbour("lower right")
+    			, "lower right");
+    	return source;
+    }
+    
+    private LinkedList<GraphPoint> getPath(GraphPoint target)
+    {
+    	LinkedList<GraphPoint> path=new LinkedList<>();
+    	GraphPoint current=target;
+    	
+    	while(current.getBestPrevious()!=null)
+    	{
+    		path.addFirst(current);
+    		current=current.getBestPrevious();
+    	}
+    	path.addFirst(current);
+    	
+    	return path;
+    }
+    
+    private void resetGraph()
+    {
+    	for(GraphPoint[] pointRow: pointMatrix)
+    	{
+    		for(GraphPoint point: pointRow)
+    		{
+    			if(point!=null)
+    			{
+    				point.reset();
+    			}
+    		}
+    	}
+    }
+}
